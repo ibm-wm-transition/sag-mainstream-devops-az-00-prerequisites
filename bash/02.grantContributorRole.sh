63c28be06fec5e914b1ea9c284f6d1c7e9000dfc
mc <118729248+mcgit70@users.noreply.github.com>
2023-09-12 14:47:51 +0000

    Microsoft declared to require the "--scope <scope path>" parameter in fall 2023. Removed "--resource-group" (for redundancy reason) and added "--scope" with corresponding value. Value for "--scope" has been built basing on a previously created RG and assigned role.

--- bash/02.grantContributorRole.sh
+++ bash/02.grantContributorRole.sh
@@ -7,7 +7,7 @@ echo "Granting Contributor role ..."
 az role assignment create \
 --assignee "$SAG_SERVICE_PRINCIPAL_ID" \
 --role "Contributor" \
---resource-group "$SAG_AZ_RG_NAME" \
---subscription "$SAG_AZ_SUBSCRIPTION_ID"
+--subscription "$SAG_AZ_SUBSCRIPTION_ID" \
+--scope "/subscriptions/$SAG_AZ_SUBSCRIPTION_ID/resourceGroups/$SAG_AZ_RG_NAME"
 
 echo "Granted Contributor role, result $?"
